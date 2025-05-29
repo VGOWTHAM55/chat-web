f08ee85995b842f15d1b2cdfd3b26ebe86965e73
GOWTHAM V <vgowtham9787@gmail.com>
2025-05-29 16:03:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -53,5 +53,9 @@ app.get("/api/messages", async (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
